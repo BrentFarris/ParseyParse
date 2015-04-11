688f270f0e55c475957413921e2108f7d9fed86e
Brent Farris <brent@farrisarts.com>
2015-04-11 11:21:26 -0700

    Parse Config

---
+++ Assets/Scripts/Parse/GameConfig.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Parse;
+using System;
+
+public class GameConfig : MonoBehaviour
+{
+	private void Start()
+	{
+		//UpdateConfig(ConfigUpdated);
+		UpdateConfig(delegate()
+		{
+			Debug.Log("Cost: " + ParseConfig.CurrentConfig.Get<int>("tierOneCoins"));
+		});
+	}
+
+	private void ConfigUpdated()
+	{
+		Debug.Log("Cost: " + ParseConfig.CurrentConfig.Get<int>("tierOneCoins"));
+	}
+
+	public void UpdateConfig(Action callback)
+	{
+		ParseConfig.GetAsync().ContinueWith(t =>
+		{
+			callback();
+		});
+	}
+}
